503e58400bd2e84372479f71adeb80e1c3d6c8b9
davidedomini <davide.domini@unibo.it>
2025-07-13 20:57:24 +0200

    fix: reduce model size

--- src/experiments/federated/utils/FedUtils.py
+++ src/experiments/federated/utils/FedUtils.py
@@ -1,5 +1,5 @@
 from experiments.common import SimpleRNN
 
-def initialize_model(hidden_size = 128, layers=4):
+def initialize_model(hidden_size = 64, layers=2):
     model = SimpleRNN(input_size=1, hidden_size=hidden_size, num_layers=layers)
     return model
