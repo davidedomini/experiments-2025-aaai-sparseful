a8d6367fae505b5f77f29c00e0d784eeb1d2bb54
Davide Domini <davide.domini@unibo.it>
2025-07-14 10:01:11 +0200

    fix: fix results folder

--- src/experiments/federated/main.py
+++ src/experiments/federated/main.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     algorithms = ['fedavg']
     data_folder = 'data/METR-LA'
     max_seed = 5
-    results_folder = 'results'
+    results_folder = 'results-metr-la'
     batch_size = 64
     local_epochs = 2
     global_rounds = 20
