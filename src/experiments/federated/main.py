2ef7e7720a73c6b014d99cbe70d1b401a0bd5b13
Davide Domini <davide.domini@unibo.it>
2025-07-14 09:46:23 +0200

    chore: update hyperparams

--- src/experiments/federated/main.py
+++ src/experiments/federated/main.py
@@ -3,13 +3,13 @@ from experiments.federated import Simulator
 
 if __name__ == '__main__':
 
-    algorithms = ['fedavg'] #fedprox, scaffold
+    algorithms = ['fedavg']
     data_folder = 'data/METR-LA'
-    max_seed = 4
+    max_seed = 5
     results_folder = 'results'
     batch_size = 64
     local_epochs = 2
-    global_rounds = 15
+    global_rounds = 20
     horizon = 1
     window_size = 20
 
